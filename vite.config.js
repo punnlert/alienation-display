007813440ebc60cb8af0f0a436d85989aa2c3e18
20210770 <punnlert@kaist.ac.kr>
2023-12-09 13:34:31 +0900

    try fix netlify error

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,7 @@ module.exports = defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        single: resolve(__dirname, 'single_sketch.html'),
-        multi: resolve(__dirname, 'multi_sketch.html')
+        main: resolve(__dirname, 'index.html')
       }
     }
   }
